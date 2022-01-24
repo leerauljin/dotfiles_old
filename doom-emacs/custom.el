06bcd37241dc22baa7b837c402bf9604c742df35
leerauljin <leerauljin@pusan.ac.kr>
2022-01-24 21:08:03 +0900

    Revert "Messed up changes"
    This reverts commit ae4d0e703900e411318569d7c05dc29a46a82dd7.

--- doom-emacs/custom.el
+++ doom-emacs/custom.el
@@ -1,24 +1,24 @@
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(package-selected-packages '(ox-odt ##)))
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(org-agenda-date ((t (:weight normal))))
-;;  '(org-agenda-date-today ((t (:weight bold :slant italic :height 1.2))))
-;;  '(org-agenda-date-weekend ((t (:weight normal))))
-;;  '(org-block-begin-line ((t (:slant italic :foreground "base3" :background "bg-alt"))))
-;;  '(org-block-end-line ((t (:slant italic))))
-;;  '(org-document-title ((t (:height 1.8 :weight bold :foreground "fg"))))
-;;  '(org-ellipsis ((t (:foreground "grey" :background "bg"))))
-;;  '(org-headline-done ((t (:strike-through t))))
-;;  '(org-hide ((t (:background "bg"))))
-;;  '(org-level-1 ((t (:height 1.6 :weight bold :slant nil :foreground "fg"))))
-;;  '(org-level-2 ((t (:height 1.3 :weight normal :slant italic :foreground "fg"))))
-;;  '(org-level-3 ((t (:height 1.2 :weight normal :slant italic :foreground "fg"))))
-;;  '(org-level-4 ((t (:height 1.1 :weight normal :slant italic :foreground "fg")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(ox-odt ##)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-date ((t (:weight normal))))
+ '(org-agenda-date-today ((t (:weight bold :slant italic :height 1.2))))
+ '(org-agenda-date-weekend ((t (:weight normal))))
+ '(org-block-begin-line ((t (:slant italic :foreground "base3" :background "bg-alt"))))
+ '(org-block-end-line ((t (:slant italic))))
+ '(org-document-title ((t (:height 1.8 :weight bold :foreground "fg"))))
+ '(org-ellipsis ((t (:foreground "grey" :background "bg"))))
+ '(org-headline-done ((t (:strike-through t))))
+ '(org-hide ((t (:background "bg"))))
+ '(org-level-1 ((t (:height 1.6 :weight bold :slant nil :foreground "fg"))))
+ '(org-level-2 ((t (:height 1.3 :weight normal :slant italic :foreground "fg"))))
+ '(org-level-3 ((t (:height 1.2 :weight normal :slant italic :foreground "fg"))))
+ '(org-level-4 ((t (:height 1.1 :weight normal :slant italic :foreground "fg")))))
