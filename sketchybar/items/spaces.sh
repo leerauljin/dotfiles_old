43953235d4e143aaa298d3901abbf09f0198f1c8
leerauljin <leerauljin@pusan.ac.kr>
2022-03-02 21:24:49 +0900

    Mar 02

--- sketchybar/items/spaces.sh
+++ sketchybar/items/spaces.sh
@@ -21,21 +21,26 @@ sketchybar --add       space              space_template left                \
                                           position=left                      \
                                           drawing=on                         \
                                                                              \
-           --clone     prod               space_template                     \
-           --set       prod               associated_space=1                 \
-                                          icon=                             \
-                                          icon.highlight_color=0xffd95468    \
-                                          drawing=on                         \
-                                                                             \
            --clone     surf               space_template                     \
-           --set       surf               associated_space=2                 \
+           --set       surf               associated_space=1                 \
                                           icon=                              \
                                           icon.highlight_color=0xffd95468    \
                                           drawing=on                         \
                                                                              \
+           --clone     prod               space_template                     \
+           --set       prod               associated_space=2                 \
+                                          icon=                             \
+                                          icon.highlight_color=0xffd95468    \
+                                          drawing=on                         \
+                                                                             \
+           --clone     ref                space_template                     \
+           --set       ref                associated_space=3                 \
+                                          icon=                            \
+                                          icon.highlight_color=0xffd95468    \
+                                          drawing=on                         \
                                                                              \
            --clone     org                space_template                     \
-           --set       org                associated_space=3                 \
+           --set       org                associated_space=4                 \
                                           icon=ﲾ                             \
                                           icon.highlight_color=0xffd95468    \
                                           drawing=on                         \
@@ -43,8 +48,9 @@ sketchybar --add       space              space_template left                \
                                                                              \
            --add       bracket            spaces_1                           \
                                           spaces_1.label                     \
-                                          prod                               \
                                           surf                                \
+                                          prod                               \
+                                          ref                               \
                                           org                                \
                                                                              \
            --set       spaces_1           background.drawing=on              \
