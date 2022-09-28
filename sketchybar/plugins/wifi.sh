103a173b94e3e5e3ded6b61bea9b4a471b27bde6
leerauljin <leerauljin@pusan.ac.kr>
2022-09-28 23:25:22 +0900

    Revised sketchybar

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -8,11 +8,11 @@ SSID="$(echo "$CURRENT_WIFI" | grep -o "SSID: .*" | sed 's/^SSID: //')"
 CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
 
 if [ "$SSID" = "" ]; then
-#   sketchybar --set $NAME label="Disconnected" icon=睊
+  sketchybar --set $NAME label="Disconnected" icon=睊
   sketchybar --set $NAME icon=睊 \
                          icon.color=$RED
 else
-#   sketchybar --set $NAME label="$SSID (${CURR_TX}Mbps)" icon=直
+  sketchybar --set $NAME label="$SSID (${CURR_TX}Mbps)" icon=直
   sketchybar --set $NAME icon=直 \
                          icon.color=$BLUE
 fi
