115fa5c0387e0bfafc28b4aa90bd3906f5621f18
leerauljin <leerauljin@pusan.ac.kr>
2022-10-28 12:08:00 +0900

    1028

--- nvchad/custom/plugins/lspconfig.lua
+++ nvchad/custom/plugins/lspconfig.lua
@@ -3,7 +3,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "pyright", "r_language_server"}
+local servers = { "pyright", "r_language_server", "gopls"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
