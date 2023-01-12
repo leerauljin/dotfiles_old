f9cb2d7c4e22f51ab33e91e7ee4941664304bf50
leerauljin <leerauljin@pusan.ac.kr>
2023-01-13 08:18:27 +0900

    Carbonfox transition

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,46 +1,6 @@
 local status, lualine = pcall(require, "lualine")
 if (not status) then return end
 
-local colors = require('everblush.core').get_colors()
-local scheme = {
-    normal = {
-        a = { fg = colors.color2, bg = colors.background },
-        b = { fg = colors.color7, bg = colors.background },
-        c = { bg = colors.background },
-    },
-
-    insert = {
-        a = { bg = colors.color4, fg = colors.background },
-        b = { fg = colors.color7, bg = colors.background },
-        c = { bg = colors.background },
-    },
-
-    command = {
-        a = { bg = colors.color1, fg = colors.background },
-        b = { fg = colors.color7, bg = colors.background },
-        c = { bg = colors.background },
-    },
-
-    visual = {
-        a = { bg = colors.color5, fg = colors.background },
-        b = { fg = colors.color7, bg = colors.background },
-        c = { bg = colors.background },
-    },
-
-    replace = {
-        a = { bg = colors.color1, fg = colors.background },
-        b = { fg = colors.color7, bg = colors.background },
-        c = { bg = colors.background },
-    },
-
-    inactive = {
-        a = { fg = colors.background, bg = colors.color7 },
-        b = { fg = colors.color7, bg = colors.background },
-    },
-}
-
-
-
 local modes = {
     ['n']   =' ',
     ['no']  =' ',
@@ -78,17 +38,17 @@ end
 lualine.setup {
     options = {
         icons_enabled = true,
-        theme = scheme,
+        theme = 'carbonfox',
         section_separators ='',
         component_separators ='',
         globalstatus = true,
     },
     sections = {
         lualine_a = { get_mode },
-        lualine_b = {'branch', 'diff', 'diagnostics' },
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = { {'filetype', icon_only = true }, 'filename', 'location'  },
+        lualine_b = {},
+        lualine_c = {'branch', 'diff', 'diagnostics' },
+        lualine_x = {{'filetype', icon_only = true }, 'filename'},
+        lualine_y = { 'location' },
         lualine_z = { }
     },
 }
