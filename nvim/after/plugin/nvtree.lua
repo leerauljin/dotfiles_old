4835baeadf9f68f2c5a39ed4d39bfd27c102fa96
leejin <sjbase@me.com>
2023-02-11 14:46:20 +0900

    minor update

--- nvim/after/plugin/nvtree.lua
+++ nvim/after/plugin/nvtree.lua
@@ -8,8 +8,6 @@ tree.setup {
     },
     disable_netrw = true,
     hijack_netrw = true,
-    open_on_setup = false,
-    ignore_ft_on_setup = { "alpha" },
     hijack_cursor = true,
     hijack_unnamed_buffer_when_opening = false,
     update_cwd = true,
