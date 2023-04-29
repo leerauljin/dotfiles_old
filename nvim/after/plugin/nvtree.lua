e5b8ed3b4fb6e3a2843569f7254a1c2f9c97c899
leerauljin <leerauljin@pusan.ac.kr>
2023-04-29 13:15:14 +0900

    Apr 29 update

--- nvim/after/plugin/nvtree.lua
+++ nvim/after/plugin/nvtree.lua
@@ -19,7 +19,6 @@ tree.setup {
         adaptive_size = true,
         side = "left",
         width = 20,
-        hide_root_folder = true,
     },
     git = {
         enable = true,
@@ -37,6 +36,7 @@ tree.setup {
     renderer = {
         -- highlight_git = false,
         highlight_opened_files = "name",
+        root_folder_label = false,
         indent_markers = {
             enable = true,
         },
